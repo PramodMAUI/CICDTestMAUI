36c593c1efd9ea87c50e561844dec1d443beeffe
CYBAGE\pramodpat25824 <pramodpat@cybage.com>
2024-01-10 15:42:13 +0530

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestCICDApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestCICDApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
